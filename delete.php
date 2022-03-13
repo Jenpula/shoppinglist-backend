dc5593669070dd470ff8f6667790bc33c23072be
Annala <n1anje00@students.oamk.fi>
2022-03-13 15:05:39 +0200

    Cleaned code and checked everything is done correct

--- delete.php
+++ delete.php
@@ -8,13 +8,13 @@ $id = filter_var($input->id, FILTER_SANITIZE_NUMBER_INT);
 try {
     $db = openDB();
 
-$query = $db->prepare('delete from item where id=(:id)');
-$query->bindValue(':id', $id, PDO::PARAM_INT);
-$query->execute();
+    $query = $db->prepare('delete from item where id=(:id)');
+    $query->bindValue(':id', $id, PDO::PARAM_INT);
+    $query->execute();
 
-header('HTTP/1.1 200 OK');
-$data = array('id' => $id);
-print json_encode($data);
+    header('HTTP/1.1 200 OK');
+    $data = array('id' => $id);
+    print json_encode($data);
 } catch (PDOException $pdoex) {
     header('HTTP/1.1 500 Internal Server Error');
     $error = array('error' => $pdoex->getMessage());
--- index.php
+++ index.php
@@ -4,13 +4,14 @@ require_once 'inc/functions.php';
 
 try {
     $db = openDB();
-  $sql = "select * from item";
-  $query = $db->query($sql);
-  $results = $query->fetchAll(PDO::FETCH_ASSOC);
-  header('HTTP/1.1 200 OK');
-  print json_encode($results);
+    $sql = "select * from item";
+    $query = $db->query($sql);
+    $results = $query->fetchAll(PDO::FETCH_ASSOC);
+    
+    header('HTTP/1.1 200 OK');
+    print json_encode($results);
 } catch (PDOException $pdoex) {
-  returnError($pdoex);
-  $error = array('error' => $pdoex->getMessage());
-  print json_encode($error);
+    returnError($pdoex);
+    $error = array('error' => $pdoex->getMessage());
+    print json_encode($error);
 }
